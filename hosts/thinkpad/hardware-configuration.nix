e703401694a647cfdaa2504ece323142413d46f3
Johannes Bartschies <johannes.bartschies@gmail.com>
2025-08-10 14:05:57 +0200

    thinkpad removed swap file because it was hella buggy

--- hosts/thinkpad/hardware-configuration.nix
+++ hosts/thinkpad/hardware-configuration.nix
@@ -45,9 +45,7 @@
     ];
   };
 
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/a6caa1ed-c793-4c00-aace-87fcec37a7d7"; }
-  ];
+  swapDevices = lib.mkForce []; #disables swap device
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
