69d5433830accdb584c2fd7ebf4c947689744edd
Johannes Bartschies <johannes.bartschies@gmail.com>
2026-01-05 21:50:31 +0100

    firewall for sunshine

--- modules/gaming/sunshine.nix
+++ modules/gaming/sunshine.nix
@@ -6,4 +6,13 @@
     capSysAdmin = true;
     openFirewall = true;
   };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 47984 47989 47990 48010 ];
+    allowedUDPPortRanges = [
+      { from = 47998; to = 48000; }
+      { from = 8000; to = 8010; }
+    ];
+  };
 }
