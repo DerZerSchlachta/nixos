c5dafc1152393f2971bb4de6486add451f98baa3
Johannes Bartschies <johannes.bartschies@gmail.com>
2026-07-12 22:31:55 +0200

    nothingphone integratino int syncthing network

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -13,11 +13,12 @@
         "FUJITSU-D3431-A" = { id = "IR4MBCI-VDGR7VV-LAD2EZG-QLNLBKD-SUWWDFJ-LL7RJL6-LADVWJP-T4O4BQD"; };
         "thinkpad-jo" = { id = "ODVSYCS-7YKGOWB-L2ZJBAD-ZUSXLZ7-MCPFZHL-HBW7PZT-G33POAX-5UTA4AW"; };
         "desktop-jo" = { id = "EGHVULC-WUNGSR3-FSHCGLE-VI7F2RO-YNRRZV3-FWVSHXZ-BGOCE6P-XQJJEQ7"; };
+        "nothing-phone" = { id = "5MSIYRP-5FZXFO5-TGZEEGX-BOLIYVF-YH3IZWD-O2NUGMK-YPS2D2P-KO7P7AN"; };
       };
       folders = {
         "z2tid-uvgee" = {         # Name of folder in Syncthing, also the folder ID - "johannes"
           path = "/home/johannes/personal-cloud/";    # Which folder to add to Syncthing
-          devices = [ "FUJITSU-D3431-A" "thinkpad-jo" "desktop-jo" ];      # Which devices to share the folder with
+          devices = [ "FUJITSU-D3431-A" "thinkpad-jo" "desktop-jo" "nothing-phone" ];      # Which devices to share the folder with
         };
       };
     };
