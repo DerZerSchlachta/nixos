309d4dd3b7299347495cdd7c27314e3e3c51b321
Johannes Bartschies <johannes.bartschies@gmail.com>
2026-07-12 22:08:03 +0200

    typo

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -11,12 +11,13 @@
     settings = {
       devices = {
         "FUJITSU-D3431-A" = { id = "IR4MBCI-VDGR7VV-LAD2EZG-QLNLBKD-SUWWDFJ-LL7RJL6-LADVWJP-T4O4BQD"; };
-        #"desktop-jo" = { id = "DEVICE-ID-GOES-HERE"; };
+        "thinkpad-jo" = { id = "ODVSYCS-7YKGOWB-L2ZJBAD-ZUSXLZ7-MCPFZHL-HBW7PZT-G33POAX-5UTA4AW"; };
+        "desktop-jo" = { id = "EGHVULC-WUNGSR3-FSHCGLE-VI7F2RO-YNRRZV3-FWVSHXZ-BGOCE6P-XQJJEQ7"; };
       };
       folders = {
         "z2tid-uvgee" = {         # Name of folder in Syncthing, also the folder ID - "johannes"
           path = "/home/johannes/personal-cloud/";    # Which folder to add to Syncthing
-          devices = [ "FUJITSU-D3431-A" ];      # Which devices to share the folder with
+          devices = [ "FUJITSU-D3431-A" "thinkpad-jo" "desktop-jo" ];      # Which devices to share the folder with
         };
       };
     };
