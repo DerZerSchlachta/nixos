615639a8dcc2f2183b864607df1d98411f362a3b
Johannes Bartschies <johannes.bartschies@gmail.com>
2026-07-16 13:52:16 +0200

    tailscale exception

--- modules/system/networking/airvpn.nix
+++ modules/system/networking/airvpn.nix
@@ -1,44 +1,58 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
-    age.secrets.airvpn-key = {
-        file = ./../../../secrets/airvpn-key.age;
-        mode = "600";
-    };
+  age.secrets.airvpn-key = {
+    file = ./../../../secrets/airvpn-key.age;
+    mode = "600";
+  };
 
-    age.secrets.airvpn-psk = {
-        file = ./../../../secrets/airvpn-psk.age;
-        mode = "600";
-    };
-    
-    networking.wg-quick.interfaces.airvpn = {
-        address = [
-            "10.141.204.161/32"
-            "fd7d:76ee:e68f:a993:2624:7456:c62e:159a/128"
+  age.secrets.airvpn-psk = {
+    file = ./../../../secrets/airvpn-psk.age;
+    mode = "600";
+  };
+
+  networking.wg-quick.interfaces.airvpn = {
+    postUp = ''
+      ${pkgs.iproute2}/bin/ip route replace \
+        100.64.0.0/10 \
+        dev tailscale0 \
+        table 51820
+    '';
+
+    postDown = ''
+      ${pkgs.iproute2}/bin/ip route del \
+        100.64.0.0/10 \
+        dev tailscale0 \
+        table 51820 || true
+    '';
+
+    address = [
+      "10.141.204.161/32"
+      "fd7d:76ee:e68f:a993:2624:7456:c62e:159a/128"
+    ];
+
+    mtu = 1320;
+
+    dns = [
+      "10.128.0.1"
+      "fd7d:76ee:e68f:a993::1"
+    ];
+
+    privateKeyFile = config.age.secrets.airvpn-key.path;
+
+    peers = [
+      {
+        publicKey = "PyLCXAQT8KkM4T+dUsOQfn+Ub3pGxfGlxkIApuig+hk=";
+        presharedKeyFile = config.age.secrets.airvpn-psk.path;
+
+        endpoint = "nl3.vpn.airdns.org:1637";
+
+        allowedIPs = [
+          "0.0.0.0/0"
+          "::/0"
         ];
 
-        mtu = 1320;
-
-        dns = [
-            "10.128.0.1"
-            "fd7d:76ee:e68f:a993::1"
-        ];
-
-        privateKeyFile = config.age.secrets.airvpn-key.path;
-
-        peers = [
-            {
-            publicKey = "PyLCXAQT8KkM4T+dUsOQfn+Ub3pGxfGlxkIApuig+hk=";
-            presharedKeyFile = config.age.secrets.airvpn-psk.path;
-
-            endpoint = "nl3.vpn.airdns.org:1637";
-
-            allowedIPs = [
-                "0.0.0.0/0"
-                "::/0"
-            ];
-
-            persistentKeepalive = 15;
-            }
-        ];
-    };
+        persistentKeepalive = 15;
+      }
+    ];
+  };
 }
